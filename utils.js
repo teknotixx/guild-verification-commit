dcffe7bdeb9647bab710b98d84144b3cf9a70f00
teknotixx <r.akkoc607@gmail.com>
2025-11-05 22:06:09 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:06:02.741Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T19:06:06.620Z
+Commit: Fix memory leak in event handlers
